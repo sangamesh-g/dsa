437e13aacf990f8147fa8d0e04e93bd496ad28c8
Gulle Sangamesh <ghullesangamesh@gmail.com>
2025-11-07 22:37:53 +0530

    binarysearchiterative

--- SearchingMethod.java
+++ SearchingMethod.java
@@ -8,10 +8,24 @@ public class SearchingMethod {
         return -1;
     }
 
+     public static int binarySearchIterative(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == target) return mid;
+            if (arr[mid] < target) low = mid + 1;
+            else high = mid - 1;
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         int[] arr = {2, 4, 6, 8, 10, 12, 14};
-        int target = 10;
+        int target = 12;
 
+        System.out.println(binarySearchIterative(arr, target));
+        System.out.println(binarySearchIterative(arr, target));
+        System.out.println(binarySearchIterative(arr, 0, arr.length - 1, target));
         System.out.println(linearSearch(arr, target));
         System.out.println(linearSearch(arr, target));
         System.out.println(linearSearch(arr, 0, arr.length - 1, target));
