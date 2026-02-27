ae20e80ee625f07af32164175e1e1b6b2a548f0c
Sangamesh Gulle <ghullesangamesh@gmail.com>
2026-02-27 08:02:29 +0530

    Implement isAnagram

---
+++ anagram/p1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        for(int i=0;i<s.length();i++){
+            int index=t.indexOf(s.charAt(i));
+            if(index==-1)return false;
+            t=t.substring(0,index)+t.substring(index+1);// substring(start,end) end not included in it
+                }
+        return true;
+    }
+}
