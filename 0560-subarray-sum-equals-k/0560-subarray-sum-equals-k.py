956513b40768af29d88ba4b1839b6f6567d08984
sangameshghulle <sangameshghulle@gmail.com>
2026-07-22 11:53:32 +0530

    Time: 27 ms (91.95%), Space: 21.9 MB (34.72%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,10 +1,19 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        prefix=0
-        cnt=0
-        mp={0:1}
-        for i in nums:
-            prefix+=i
-            cnt+=mp.get(prefix-k,0)
-            mp[prefix]=mp.get(prefix,0)+1
-        return cnt
+        currentSum = 0
+        preSumMap = {0:1}
+        result = 0
+
+        for num in nums:
+            currentSum += num 
+            
+            requiredSum = currentSum - k
+            if requiredSum in preSumMap:
+                result += preSumMap[requiredSum]
+            
+            if currentSum in preSumMap:
+                preSumMap[currentSum] += 1
+            else:
+                preSumMap[currentSum] = 1
+            
+        return result
