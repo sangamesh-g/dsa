2cba5a9a47988ce5e0a8e518f1ef6d014eb76731
sangameshghulle <sangameshghulle@gmail.com>
2026-07-27 08:49:33 +0530

    Time: 7 ms (38.22%), Space: 19.9 MB (20.76%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -2,20 +2,16 @@ from typing import List
 
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        max_end = nums[0]   # Maximum product ending here
-        min_end = nums[0]   # Minimum product ending here
-        ans = nums[0]
+        maxend=nums[0]
+        minend=nums[0]
+        ans=nums[0]
 
-        for x in nums[1:]:
-
-            # Negative number flips max and min
-            if x < 0:
-                max_end, min_end = min_end, max_end
-
-            # Either start a new subarray or extend the previous one
-            max_end = max(x, max_end * x)
-            min_end = min(x, min_end * x)
-
-            ans = max(ans, max_end)
-
+        for n in nums[1:]:
+            if n<0:
+                maxend,minend=minend,maxend
+            
+            maxend=maxend*n if maxend*n>n else n
+            minend=minend*n if minend*n<n else n
+            if ans<maxend:
+                ans=maxend
         return ans
