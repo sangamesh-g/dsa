2b519ac19143c7fc2d5fcc06769752bd439aecdb
sangameshghulle <sangameshghulle@gmail.com>
2026-07-28 08:46:05 +0530

    Time: 0 ms (100%), Space: 19.1 MB (98.19%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -4,6 +4,6 @@ class Solution:
         while i<len(nums):
             if nums[i]==val:
                 del nums[i]
-                i-=1
-            i+=1
+            else:
+                i+=1
         return len(nums)
