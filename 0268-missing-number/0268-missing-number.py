85890f02cb22caa87512e8faa70bf6c47426c265
sangameshghulle <sangameshghulle@gmail.com>
2026-07-31 09:57:18 +0530

    Time: 3 ms (59.43%), Space: 21 MB (9.47%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,6 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        seen=set(nums)
+        for i in range(len(nums)+1):
+            if i not in seen:
+                return i
