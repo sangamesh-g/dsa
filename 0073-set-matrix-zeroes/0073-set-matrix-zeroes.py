e0eedc84bd85b8273f0f81c091ff1324c7a56c71
sangameshghulle <sangameshghulle@gmail.com>
2026-08-06 10:59:05 +0530

    Time: 0 ms (100%), Space: 20.9 MB (19.68%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -4,21 +4,31 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         rowlen=len(matrix)
-        rows=[False]*rowlen
         collen=len(matrix[0])
-        cols=[False]*collen
-        for i in range(rowlen):
-            for j in range(collen):
+        is_first_col_zero=False
+        is_first_row_zero=False
+        for i in range(collen):
+            if matrix[0][i]==0:
+                is_first_row_zero=True
+        for j in range(rowlen):
+            if matrix[j][0]==0:
+                is_first_col_zero=True
+
+        for i in range(1,rowlen):
+            for j in range(1,collen):
                 if matrix[i][j]==0:
-                    rows[i]=True
-                    cols[j]=True
+                    matrix[i][0]=0
+                    matrix[0][j]=0
+        
+        for i in range(1, rowlen):
+            for j in range(1, collen):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
 
-        for i in range(rowlen):
-            if rows[i]==True:
-                for k in range(collen):
-                    matrix[i][k]=0
+        if is_first_row_zero:
+            for k in range(collen):
+                matrix[0][k]=0
 
-        for j in range(collen):
-            if cols[j]==True:
-                for k in range(rowlen):
-                    matrix[k][j]=0
+        if is_first_col_zero:
+            for k in range(rowlen):
+                matrix[k][0]=0
