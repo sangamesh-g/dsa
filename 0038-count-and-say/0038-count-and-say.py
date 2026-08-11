21b28ca14733520dd6edaa6c8be8d3886fb965a6
sangameshghulle <sangameshghulle@gmail.com>
2026-08-11 09:28:17 +0530

    Time: 7 ms (72.14%), Space: 19.3 MB (36.79%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -3,15 +3,16 @@ class Solution:
         
         prev='1'
         for _ in range(n-1):
-            result=''
+            result=[]
             count=1
             length=len(prev)
             for i in range(length):
                 if i<length-1 and prev[i]==prev[i+1]:
                     count+=1
                 else:
-                    result+=str(count)+prev[i]
+                    result.append(str(count))
+                    result.append(prev[i])
                     count=1
-            prev=result
+            prev="".join(result)
             
         return prev
