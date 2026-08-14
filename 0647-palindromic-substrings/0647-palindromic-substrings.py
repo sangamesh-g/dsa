30d27e770f7bbce5f4756b4c182f42ec91efa3b6
sangameshghulle <sangameshghulle@gmail.com>
2026-08-14 08:52:53 +0530

    Time: 812 ms (5.5%), Space: 19.3 MB (37.87%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,40 +1,42 @@
-# class Solution:
-#     def countSubstrings(self, s: str) -> int:
-#         ans=len(s)
-#         n=ans
-#         for i in range(n):
-#             j=1
-#             while i-j>=0 and i+j<n and s[i-j]==s[i+j]:
-#                 print(s[i-j],s[i+j],ans)
-#                 ans+=1
-#                 j+=1
-            
-#             j=i
-#             k=i+1
-#             while j>=0 and k<n and s[j]==s[k]:
-#                 print(s[j],s[k],ans)
-#                 ans+=1
-#                 j-=1
-#                 k+=1
-        
-#         return ans
-
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        count = 0 
+        n=len(s)
+        ans=0
+        for i in range(n):
+            left=i
+            right=i
+            while left>=0 and right<n and s[left]==s[right]:
+                print(s[left],s[right],ans)
+                ans+=1
+                left-=1
+                right+=1
+            
+            left_=i
+            right_=i+1
+            while left_>=0 and right_<n and s[left_]==s[right_]:
+                print(s[left_],s[right_],ans)
+                ans+=1
+                left_-=1
+                right_+=1
+        
+        return ans
 
-        for i in range(len(s)):
-            count += self.func(i, i , s)
-            count += self.func(i, i+1 , s)
+# class Solution:
+#     def countSubstrings(self, s: str) -> int:
+#         count = 0 
+
+#         for i in range(len(s)):
+#             count += self.func(i, i , s)
+#             count += self.func(i, i+1 , s)
         
-        return count
+#         return count
     
-    @staticmethod
-    def func(left , right , s):
-        count = 0
-        while left >= 0 and right < len(s) and s[left] == s[right]:
-            count += 1
-            left -= 1
-            right += 1
+#     @staticmethod
+#     def func(left , right , s):
+#         count = 0
+#         while left >= 0 and right < len(s) and s[left] == s[right]:
+#             count += 1
+#             left -= 1
+#             right += 1
         
-        return count
+#         return count
