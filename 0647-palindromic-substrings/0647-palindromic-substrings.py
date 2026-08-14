3156771e7fdae3bdb4b6e261196f209f15411795
sangameshghulle <sangameshghulle@gmail.com>
2026-08-14 08:46:18 +0530

    Time: 115 ms (79.92%), Space: 19.2 MB (91.8%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,20 +1,40 @@
+# class Solution:
+#     def countSubstrings(self, s: str) -> int:
+#         ans=len(s)
+#         n=ans
+#         for i in range(n):
+#             j=1
+#             while i-j>=0 and i+j<n and s[i-j]==s[i+j]:
+#                 print(s[i-j],s[i+j],ans)
+#                 ans+=1
+#                 j+=1
+            
+#             j=i
+#             k=i+1
+#             while j>=0 and k<n and s[j]==s[k]:
+#                 print(s[j],s[k],ans)
+#                 ans+=1
+#                 j-=1
+#                 k+=1
+        
+#         return ans
+
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        ans=len(s)
-        n=ans
-        for i in range(n):
-            j=1
-            while i-j>=0 and i+j<n and s[i-j]==s[i+j]:
-                print(s[i-j],s[i+j],ans)
-                ans+=1
-                j+=1
-            
-            j=i
-            k=i+1
-            while j>=0 and k<n and s[j]==s[k]:
-                print(s[j],s[k],ans)
-                ans+=1
-                j-=1
-                k+=1
+        count = 0 
+
+        for i in range(len(s)):
+            count += self.func(i, i , s)
+            count += self.func(i, i+1 , s)
         
-        return ans
+        return count
+    
+    @staticmethod
+    def func(left , right , s):
+        count = 0
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            count += 1
+            left -= 1
+            right += 1
+        
+        return count
