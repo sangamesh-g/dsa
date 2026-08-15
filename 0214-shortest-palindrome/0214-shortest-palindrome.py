d2c98444e92367d3ef1fa7d889b71d4863870a3e
sangameshghulle <sangameshghulle@gmail.com>
2026-08-15 08:31:36 +0530

    Time: 103 ms (26.13%), Space: 19.8 MB (77.13%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -0,0 +1,33 @@
+class Solution:
+    # def palin(self,s:str):
+    #     left=0
+    #     right=len(s)-1
+    #     while left < right:
+    #         if s[left] != s[right]:
+    #             return False
+    #         left += 1
+    #         right -= 1
+    #     return True
+
+    # def shortestPalindrome(self, s: str) -> str:
+        
+    #     n=len(s)
+    #     r=n-1
+    #     while r>=0:
+    #         if self.palin(s[:r+1]):
+    #             break
+    #         r-=1
+    #     sufix=s[r+1:]
+    #     return sufix[::-1]+s
+
+    def shortestPalindrome(self, s: str) -> str:
+        rev=s[::-1]
+        n=len(s)
+        r=0
+        for r in range(n):
+            if s[:n-r]==rev[r:]:
+                # print(s[:n-r],rev[r:],r)
+                break
+        t=rev[:r]
+        # print(t)
+        return t+s
