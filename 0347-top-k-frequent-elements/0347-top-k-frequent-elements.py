c26e7568ec92d5b7cfc8c1300312a82ad6d6ff55
sangameshghulle <sangameshghulle@gmail.com>
2026-08-16 08:59:46 +0530

    Time: 7 ms (51.9%), Space: 22.8 MB (60.2%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,7 +1,6 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         dic=Counter(nums)
-        ans=sorted(dic.items(),key=lambda x:x[1],reverse=True)
-        # print(ans)
+        ans=dic.most_common(k)
 
-        return [x[0] for x in ans[:k]]
+        return [x[0] for x in ans]
