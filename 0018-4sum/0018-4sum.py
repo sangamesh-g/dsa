4b805524fcbbc1f2283dcf4b052def7a9c741dba
Sangamesh Gulle <ghullesangamesh@gmail.com>
2026-08-18 09:51:33 +0530

    Time: 44 ms (91.47%), Space: 19.3 MB (53.61%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -4,7 +4,11 @@ class Solution:
         ans=[]
         nums.sort()
         for i in range(n-3):
-            if i>0 and nums[i]==nums[i-1]:
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            if nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target:
+                break
+            if nums[i] + nums[n - 3] + nums[n - 2] + nums[n - 1] < target:
                 continue
             for j in range(i+1,n-2):
                 if j>i+1 and nums[j]==nums[j-1]:
